89760a3bfa62e2458c46ffaed197ebbd910910d9
Remo Albisser <remoalbisser@bluewin.ch>
2024-11-18 09:28:57 +0100

    go-2-ex-1/main.go

--- go-2-ex-1/main.go
+++ go-2-ex-1/main.go
@@ -3,26 +3,42 @@ package main
 import "fmt"
 
 type FullName struct {
-	// TODO: add fields
+	FirstName string
+	LastName  string
 }
 
-// TODO: declare a structure for birth date
+type BirthDate struct {
+	Day   byte
+	Month byte
+	Year  int
+}
 
 type Profile struct {
-	// TODO: embed full name and birth date information
+	Name             FullName
+	Birth            BirthDate
 	NumberOfSiblings byte
 	ZodiacSign       rune
 }
 
 func main() {
+
 	var me = Profile{
-		// TODO: set name and birth date information
-		NumberOfSiblings: 0,   // TODO: adjust
-		ZodiacSign:       ' ', // TODO: adjust
+		Name: FullName{
+			FirstName: "Remo",
+			LastName:  "Albisser",
+		},
+		Birth: BirthDate{
+			Day:   1,
+			Month: 4,
+			Year:  2007,
+		},
+		NumberOfSiblings: 2,
+		ZodiacSign:       '\u2651',
 	}
-	fmt.Println(me)
+
+	fmt.Println("Steckbrief:", me)
 
 	fmt.Println("Siblings Before:", me.NumberOfSiblings)
-	// TODO: imagine, you get a little brother or sister
+	me.NumberOfSiblings++
 	fmt.Println("Siblings After:", me.NumberOfSiblings)
 }
