3d446e6cd1f9dadb11dcc65e37bc40d52ceba7e6
Remo Albisser <remoalbisser@bluewin.ch>
2024-11-25 08:43:20 +0100

    go-2-ex-2

--- go-2-ex-2/main.go
+++ go-2-ex-2/main.go
@@ -5,11 +5,13 @@ import "fmt"
 func main() {
 	var fibs = []int{1, 1, 0, 0, 0}
 
-	fibs[2] = fibs[0] + fibs[1]
-	// TODO: correct up to index 4 using direct element access
+	for i := 2; i < len(fibs); i++ {
+		fibs[i] = fibs[i-1] + fibs[i-2]
+	}
 
-	fibs = append(fibs, 0) // TODO: replace 0 with the next Fibonacci number
-	// TODO: compute three more Fibonacci numbers and append them
+	for i := 0; i < 4; i++ {
+		fibs = append(fibs, fibs[len(fibs)-1]+fibs[len(fibs)-2])
+	}
 
-	fmt.Println(fibs) // expected output: [1 1 2 3 5 8 13 21 34]
+	fmt.Println(fibs)
 }
