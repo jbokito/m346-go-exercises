d74fb002b9f3f12f6fb1f521a62792d05ce8d954
Remo Albisser <remoalbisser@bluewin.ch>
2024-11-25 09:16:34 +0100

    go-2-ex-3

--- go-2-ex-3/main.go
+++ go-2-ex-3/main.go
@@ -3,14 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	// TODO: create a map called "modules"
+
+	modules := map[int]string{
+		104: "Datenmodell implementieren",
+		117: "Informatik- und Netzinfrastruktur für ein kleines Unternehmen realisieren",
+		346: "Cloud-Lösungen konzipieren und realisieren",
+	}
 
 	fmt.Println("Modul 104:", modules[104])
 	fmt.Println("Modul 117:", modules[117])
 	fmt.Println("Modul 346:", modules[346])
 
-	// TODO: delete one
-	// TODO: add one
-	// TODO: replace one
+	delete(modules, 117)
+
+	modules[200] = "Mobile Apps entwickeln"
+
+	modules[346] = "Cloud-Lösungen optimieren"
+
 	fmt.Println(modules)
 }
