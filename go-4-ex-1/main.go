fca484af7bdc062f771b1784d26b6623cebe31ed
Remo Albisser <remoalbisser@bluewin.ch>
2024-12-09 08:46:20 +0100

    go-4-ex-1

--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,7 +1,45 @@
 package main
 
-// TODO: implement the function computeGrade
+import (
+	"errors"
+	"fmt"
+)
+
+// Zusatzaufgabe
+func computeGrade(gotPoints, maxPoints float64) (float64, error) {
+	if gotPoints < 0 || maxPoints <= 0 {
+		return 0, errors.New("invalid input: points cannot be negative and maxPoints must be positive")
+	}
+	if gotPoints > maxPoints {
+		return 0, errors.New("invalid input: gotPoints cannot exceed maxPoints")
+	}
+
+	note := 1.0 + 5.0*(gotPoints/maxPoints)
+	if note > 6.0 {
+		note = 6.0
+	}
+	return note, nil
+}
 
 func main() {
-	// TODO: call the function computeGrade
+	grade1, err1 := computeGrade(17.5, 28.0)
+	if err1 != nil {
+		fmt.Println("Error:", err1)
+	} else {
+		fmt.Printf("computeGrade(17.5, 28.0) = %.3f\n", grade1)
+	}
+
+	grade2, err2 := computeGrade(28.0, 28.0)
+	if err2 != nil {
+		fmt.Println("Error:", err2)
+	} else {
+		fmt.Printf("computeGrade(28.0, 28.0) = %.3f\n", grade2)
+	}
+
+	grade3, err3 := computeGrade(-5.0, 28.0)
+	if err3 != nil {
+		fmt.Println("Error:", err3)
+	} else {
+		fmt.Printf("computeGrade(-5.0, 28.0) = %.3f\n", grade3)
+	}
 }
